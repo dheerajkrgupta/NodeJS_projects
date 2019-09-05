1dc3b34ae59ff8b33dcd9c40b2072cd083575a27
Dheeraj Gupta <mediamaster.in@gmail.com>
2019-09-05 14:35:43 +0530

    Updated by Dheeraj

--- HTMLContentCloning/index.js
+++ HTMLContentCloning/index.js
@@ -3,26 +3,31 @@
 	var fs = require('fs');
 	fs.readdir("./source/",function(err, files){
 		if (err) { return console.error(err);}
-		files.forEach( function (file)
-		{
+		files.forEach( function (file){
 		var sPath = "./source/"+file;
 		var tPath = "./target/"+file;
-		
 /* Reading Source file */
-	fs.readFile(sPath,'utf8', function(err, data) {
-		var startIndex1 = data.indexOf("<body>");
-		var lastIndex1 = data.indexOf("</body>");
-		var fileContent1 = data.substring(startIndex1+6,lastIndex1);
-
+		var sData = fs.readFileSync(sPath,'utf8');
+		var startIndex1 = sData.indexOf("</head>");
+		var lastIndex1 = sData.indexOf("</body>");
+			if(startIndex1!=-1 && lastIndex1!=-1){ 
+				var fileContent1 = sData.substring(startIndex1+7,lastIndex1);
+			}
+			else{
+					console.log("HTML Tages are missing in Source "+file);
+				}
   /* Reading target file */
-	fs.readFile(tPath,'utf8', function(err, data) {  
-		var startIndex2 = data.indexOf("<body>");
-		var lastIndex2 = data.indexOf("</body>");
-		var fileContent2 = data.substring(startIndex2+6,lastIndex2);
-		var newdata = data.replace( fileContent2, fileContent1);
-		fs.writeFileSync(tPath, newdata);
-		console.log(file+' is cloned in target folder successfully.');
-   });  
- });
+		var tData = fs.readFileSync(tPath,'utf8'); 
+		var startIndex2 = tData.indexOf("</head>");
+		var lastIndex2 = tData.indexOf("</body>");
+			if(startIndex2!=-1 && lastIndex2!=-1){ 
+				var fileContent2 = tData.substring(startIndex2+7,lastIndex2);
+				var newdata = tData.replace( fileContent2, fileContent1);
+				fs.writeFileSync(tPath, newdata);
+				console.log(file+' is cloned in target folder successfully.');
+			}
+			else{
+				console.log("HTML Tages are missing in target "+file);
+			}
  });
 });
