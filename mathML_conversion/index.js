98603827ca555d42471df423f78e267b2b385812
Dheeraj Gupta <mediamaster.in@gmail.com>
2019-12-08 21:41:05 +0530

    command line loader added by dheeraj

--- mathML_conversion/index.js
+++ mathML_conversion/index.js
@@ -1,31 +1,40 @@
-/* Author: Dheeraj Kr. Gupta */
-
 const fs = require('fs');
 const Translator=require("latextomathml");
 const readdir = require("recursive-readdir");
 const replaceall = require("replaceall");
+const cliProgress = require('cli-progress');
+const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
 const LogfileName =  "Log_file_" + new Date().getTime() + ".txt";
 
 readdir("./QTI/").then( function(files) {
 	let counter = 0;
 	let totalFiles = files.length; 
+	console.log(" Total File(s): "+ totalFiles);
+	
 	if (totalFiles > 0)
 	{ 
-	console.log(" Total File(s): "+ totalFiles);
+	let fCount = 0;
+	bar1.start(totalFiles, 0);	
 	files.forEach( function (file)
 		{
-				 let ndata, fdata, sIndex, d, bsIndex, mEquation, upEquation, mathmlstr;
-                 let quesXml = fs.readFileSync(file,'utf8');
-                 let latexPat1 = quesXml.indexOf("\\(");
-                 let latexPat2 = quesXml.indexOf("\\)");
+				
+				 let ndata, fdata, sIndex, bsIndex, mEquation, upEquation, mathmlstr;
 				 let xfileType = file.split(".");
-
-            if(latexPat1 !=-1 && latexPat2 !=-1 && xfileType[1] == "xml")
-				{			
-					ndata = replaceall("\\\\(", "<dheerajG>", quesXml);
-                    fdata = replaceall("\\\\)","</dheerajG>", ndata);
-					let patLength = fdata.match(/<dheerajG>/g).length;
+				 fCount++;	
+				 bar1.update(fCount);
+            if(xfileType[1] == "xml")
+				{
+						fs.appendFileSync( LogfileName , ""+ file +" is being loading...\n", function (err) {
+						if (err) throw err; });
+						let quesXml = fs.readFileSync(file,'utf8');
+						let latexPat1 = quesXml.indexOf("\\(");
+						let latexPat2 = quesXml.indexOf("\\)");
+						
+						if (latexPat1 !=-1 && latexPat2 !=-1) {
+							ndata = replaceall("\\\\(", "<dheerajG>", quesXml);
+							fdata = replaceall("\\\\)","</dheerajG>", ndata);
+							let patLength = fdata.match(/<dheerajG>/g).length;
 					
 					while (patLength > 0)
 					{
@@ -37,8 +46,9 @@ readdir("./QTI/").then( function(files) {
 							{							
 								//console.log("*********** MathML Conversion ***********");
 								upEquation = replaceall("\\\\", "\\", mEquation);
+								 
+								 
 								mathmlstr=Translator.LaTeXtoMathML(upEquation);
-								//console.log("mathmlstr: "+mathmlstr);
 								fdata = replaceall("<dheerajG>"+mEquation+"</dheerajG>", mathmlstr, fdata);
 															
 							}
@@ -60,6 +70,7 @@ readdir("./QTI/").then( function(files) {
 								//Replacing symbols in MathML  
 								fdata = replaceall("<mo>#</mo><mn>60</mn><mo>;</mo>","<mo>&lt;</mo>", fdata);
 								fdata = replaceall("<mo>#</mo><mn>62</mn><mo>;</mo>","<mo>&gt;</mo>", fdata);
+								fdata = replaceall("<mo>#</mo><mn>44</mn><mo>;</mo>","<mo>,</mo>", fdata);
 								fs.writeFileSync(file, fdata);	
 								}
                      }
@@ -67,15 +78,26 @@ readdir("./QTI/").then( function(files) {
 							fs.appendFileSync( LogfileName , ""+ file +" is modified for MathML conversion\n", function (err) {
 							if (err) throw err; });	
 							counter++;
+							
+						}
+								
+				else {
+					fs.appendFileSync( LogfileName , ""+ file +" is not modified.\n", function (err) {
+					if (err) throw err; });
 				}
+							
+							
+				}
+				
 					if(totalFiles==1 && counter >= 1)
 					{
+					bar1.stop();
 					console.log(" Total Modified File(s): "+ counter);
 					console.log(" MathML conversion has been done. Please check "+LogfileName+" for modified XML file(s).");
 					}
 					else if(totalFiles==1 && counter==0)
 					{
-					console.log(" We did not find any math equation in xml file(s) for MathML conversion.");
+					console.log("\n We did not find any math equation in xml file(s) for MathML conversion.");
 					}
 				else
 				{
