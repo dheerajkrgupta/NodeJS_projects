4339607e21b7acbea9f503b6899969d3aca955a9
Dheeraj Gupta <mediamaster.in@gmail.com>
2020-09-20 11:34:12 +0530

    Files updated by dheeraj kumar

--- Image_compress/index.js
+++ Image_compress/index.js
@@ -5,6 +5,9 @@ const Jimp = require('jimp');
 const fs = require("fs");
 const path = require("path");
 
+let minFileSize = 50;  /* Minimum file size (KB) for optimization */
+let counter = 0;
+
 const localFiles = function(dirPath, arrayOfFiles) {
   files = fs.readdirSync(dirPath)
   arrayOfFiles = arrayOfFiles || []
@@ -22,12 +25,19 @@ const localFiles = function(dirPath, arrayOfFiles) {
 
 const imgFileArray = localFiles("images/");
 
+console.log("Total files: "+  imgFileArray.length);
+console.log("Image files (jpg/png) will be optimized which are greater than ("+minFileSize+" KB) in file size.");
+
 for(let i=0; imgFileArray.length > i; i++) {
 	if(imgFileArray[i].indexOf(".png") != -1) {
 		let uPath = imgFileArray[i].toString();
 		let uPath2 = uPath.replace(/\\/g, "/");
 		let lastIndex = uPath2.lastIndexOf("/");
 		let uPath3 = uPath2.substring(0, lastIndex).replace(/images/g, "build");
+		let cFile = Math.round(fs.statSync(uPath2).size / 1024);  
+			
+		if( cFile > minFileSize) {
+		counter++
 		const files = imagemin([uPath2], {
         destination: uPath3,
         plugins: [
@@ -36,25 +46,37 @@ for(let i=0; imgFileArray.length > i; i++) {
             })
         ]
     });
+	}
 }
 	 
 	if( i == imgFileArray.length-1) { 
+	console.log("Total PNG files are being optimized:  "+  counter);
 	setTimeout(function(){ jpgOptimize();}, 1600);
 	}
 }
 
 function jpgOptimize() {
+	
+	counter = 0;
 	for(let j=0; imgFileArray.length > j; j++) {
 		if (imgFileArray[j].indexOf(".jpg") != -1 ) {
 		let jPath = imgFileArray[j].toString();
 		let jPath2 = jPath.replace(/\\/g, "/");
 		let jPath3 = jPath2.replace(/images/g, "build");
+		let cjFile = Math.round(fs.statSync(jPath2).size / 1024);
+		
+		if( cjFile > minFileSize) { 
+		counter++;
 		Jimp.read(jPath2, (err, dkg) => {
 			if (err) throw err;
 			dkg
-			.quality(50) // set JPEG quality
-			.write(jPath3); // save
+			.quality(50)
+			.write(jPath3); 
 			});
+			}
 		}
+			if( j == imgFileArray.length-1) { 
+				console.log("Total JPG files are being optimized:  "+  counter);
+			}
 	}
 }
