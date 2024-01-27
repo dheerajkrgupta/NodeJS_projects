28f87416723f947b66ad73d113c631cc36d011c1
Dheeraj Kumar Gupta <mediamaster.in@gmail.com>
2024-01-27 18:15:09 +0530

    File updated.

--- Routing/app.js
+++ Routing/app.js
@@ -1,1 +1,7 @@
+const express = require('express');
+const {router} = require('./routes/approutes'); 
+const app = express();
+app.use('/', router);
 
+const port = 3000;
+app.listen(port, ()=> { console.log("server is started now." )});
